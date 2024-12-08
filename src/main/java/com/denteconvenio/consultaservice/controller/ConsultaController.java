606742d89f9517a8e9cbff5c2124bc93ea30ad57
Gabriel Tessaro Favareto <gabrieltessarofavareto@gmail.com>
2024-12-08 02:51:35 -0300

    att 1,1

--- src/main/java/com/denteconvenio/consultaservice/controller/ConsultaController.java
+++ src/main/java/com/denteconvenio/consultaservice/controller/ConsultaController.java
@@ -7,7 +7,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -39,11 +38,6 @@ public class ConsultaController {
         service.agendarConsulta(request, dto);
     }
 
-    @PatchMapping
-    @PreAuthorize("hasAnyRole('BENEFICIARIO', 'CONSULTORIO')")
-    public void alterarConsulta(HttpServletRequest request){
-        
-    }
     
     @DeleteMapping("/{idConsulta}")
     @PreAuthorize("hasAnyRole('BENEFICIARIO', 'CONSULTORIO')")
